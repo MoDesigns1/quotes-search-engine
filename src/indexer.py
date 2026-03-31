b34b02fb0844aa23ef9a7b1c78fe44d4a39019b2
Mohab Elhasade <mohabthe@gmail.com>
2026-03-31 21:33:09 +0100

    Add build_index for inverted index

--- src/indexer.py
+++ src/indexer.py
@@ -4,3 +4,24 @@ import re
 def tokenize(text):
     words = re.findall(r"[a-z]+", text.lower())
     return words
+
+
+def build_index(crawl_results):
+    # crawl_results is a list of {"url": ..., "text": ...} dicts
+    index = {}
+
+    for doc in crawl_results:
+        url = doc["url"]
+        words = tokenize(doc["text"])
+
+        for position, word in enumerate(words):
+            if word not in index:
+                index[word] = {}
+
+            if url not in index[word]:
+                index[word][url] = {"freq": 0, "positions": []}
+
+            index[word][url]["freq"] += 1
+            index[word][url]["positions"].append(position)
+
+    return index
